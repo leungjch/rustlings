55953de1a04088891bcd19d457addd19fa335302
Justin <justin.leung.ch@gmail.com>
2020-12-20 19:19:16 -0500

    Hashmaps and vectors

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,19 +11,19 @@
 // Execute the command `rustlings hint collections3` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
-
-    basket
+    basket.insert(String::from("orange"), 5);
+    basket.insert(String::from("apple"), 3);
+    return basket;
 }
 
 #[cfg(test)]
--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -36,8 +35,9 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        basket.insert(Fruit::Pineapple, 10);
+        basket.insert(Fruit::Banana, 10);
+
     }
 }
 
